663a715bd2a55456ecb4b08aa2e18b8be9a3d7a7
李祖明 <solace.lee@outlook.com>
2023-10-09 09:17:25 +0800

    feat: 计算缩放比例

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,9 @@ extern crate serde_json;
 use bilinear_interpolation::init_data::init_json::ImageData;
 
 fn main() {
-    let image_data = init_json::ImageData::new("../json/image.json").expect("出现错误");
+    let image_data = ImageData::new("./json/RT_fmt.json").expect("出现错误");
+    let scaleW = image_data.dst_width as f32 / image_data.width as f32;
+    let scaleH = image_data.dst_height as f32 / image_data.height as f32;
+
+    print!("缩放比例：宽{:#}，高{:#}", scaleW, scaleH)
 }
